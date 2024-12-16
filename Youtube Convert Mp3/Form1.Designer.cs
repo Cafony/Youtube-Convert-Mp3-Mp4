971eb75d4969968fceb4a7925a1f96bc9879d2ba
Cafony <tozecerdeira@hotmail.com>
2024-12-16 12:51:20 +0000

    Add Normalize

--- Youtube Convert Mp3/Form1.Designer.cs
+++ Youtube Convert Mp3/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.labelStatus = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,19 +38,21 @@
             this.labelPasteLink = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxNormalize = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonConvert
             // 
-            this.button1.Location = new System.Drawing.Point(53, 157);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Download";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonConvert.Location = new System.Drawing.Point(53, 157);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(83, 26);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
             // 
             // textBox1
             // 
@@ -59,14 +61,14 @@
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(53, 125);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(630, 26);
+            this.textBox1.Size = new System.Drawing.Size(674, 26);
             this.textBox1.TabIndex = 1;
             // 
             // labelStatus
             // 
             this.labelStatus.AutoSize = true;
             this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelStatus.Location = new System.Drawing.Point(49, 102);
+            this.labelStatus.Location = new System.Drawing.Point(133, 102);
             this.labelStatus.Name = "labelStatus";
             this.labelStatus.Size = new System.Drawing.Size(15, 20);
             this.labelStatus.TabIndex = 2;
@@ -78,15 +80,15 @@
             this.label1.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(56, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(333, 34);
+            this.label1.Size = new System.Drawing.Size(421, 34);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Youtube Video To Mp3";
+            this.label1.Text = "Youtube Converter Mp3/Mp4";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(633, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(677, 26);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(50, 50);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -97,18 +99,17 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(491, 63);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(575, 79);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(192, 16);
+            this.label2.Size = new System.Drawing.Size(152, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Using, YoutubeExplode Library";
             // 
             // labelPasteLink
             // 
-            this.labelPasteLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.labelPasteLink.AutoSize = true;
-            this.labelPasteLink.Location = new System.Drawing.Point(606, 109);
+            this.labelPasteLink.Location = new System.Drawing.Point(50, 107);
             this.labelPasteLink.Name = "labelPasteLink";
             this.labelPasteLink.Size = new System.Drawing.Size(77, 13);
             this.labelPasteLink.TabIndex = 6;
@@ -121,38 +122,60 @@
             this.comboBox1.Items.AddRange(new object[] {
             "mp3",
             "mp4"});
-            this.comboBox1.Location = new System.Drawing.Point(388, 173);
+            this.comboBox1.Location = new System.Drawing.Point(654, 162);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(108, 21);
+            this.comboBox1.Size = new System.Drawing.Size(73, 21);
             this.comboBox1.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(388, 157);
+            this.label4.Location = new System.Drawing.Point(538, 167);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.Size = new System.Drawing.Size(110, 13);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Convert to: Video/Audio:";
+            this.label4.Text = "Format : Video/Audio:";
             // 
-            // checkBox1
+            // label3
             // 
-            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(513, 177);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(170, 17);
-            this.checkBox1.TabIndex = 9;
-            this.checkBox1.Text = "Open output Folder in the end.";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(645, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "by Antony Code";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Output Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBoxNormalize
+            // 
+            this.checkBoxNormalize.AutoSize = true;
+            this.checkBoxNormalize.Location = new System.Drawing.Point(232, 167);
+            this.checkBoxNormalize.Name = "checkBoxNormalize";
+            this.checkBoxNormalize.Size = new System.Drawing.Size(110, 17);
+            this.checkBoxNormalize.TabIndex = 12;
+            this.checkBoxNormalize.Text = "Normalize Volume";
+            this.checkBoxNormalize.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 251);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(784, 230);
+            this.Controls.Add(this.checkBoxNormalize);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.labelPasteLink);
@@ -161,7 +184,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.labelStatus);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonConvert);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -175,7 +198,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonConvert;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label labelStatus;
         private System.Windows.Forms.Label label1;
@@ -184,7 +207,9 @@
         private System.Windows.Forms.Label labelPasteLink;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBoxNormalize;
     }
 }
 
--- Youtube Convert Mp3/Form1.cs
+++ Youtube Convert Mp3/Form1.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 using YoutubeExplode.Converter;
+using System.Diagnostics;
 
 
 namespace Youtube_Convert_Mp3
@@ -19,19 +20,24 @@ namespace Youtube_Convert_Mp3
     public partial class Form1 : Form
     {
         // Set the output directory path here
-        private readonly YoutubeClient youtube = new YoutubeClient();
-
+        private readonly YoutubeClient youtube = new YoutubeClient();        
+        private string videoUrl;
+        private string _format;
+        private string _outputFilePath;
+        //Add ffmpeg file into the aplication instalation folder
+        private string _ffmpegPath = Path.Combine(Application.StartupPath, "ffmpeg.exe");
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+
+
+        private async void buttonConvert_Click(object sender, EventArgs e)
         {
-            string videoUrl = textBox1.Text;
-            var youtube = new YoutubeClient();
-            string _format = comboBox1.Text;
+            videoUrl = textBox1.Text;
+            _format = comboBox1.Text;
 
             // Create a SaveFileDialog to prompt the user for a save location
             using (SaveFileDialog saveFileDialog = new SaveFileDialog())
@@ -44,52 +50,116 @@ namespace Youtube_Convert_Mp3
                     // Get video information
                     var video = await youtube.Videos.GetAsync(videoUrl);
                     var title = video.Title;
-
-                    saveFileDialog.FileName = $"{title}.{_format}"; // Default file name
+                    saveFileDialog.FileName = $"{title}.{_format}"; // Get Default file name
 
 
                     if (saveFileDialog.ShowDialog() == DialogResult.OK) {
 
                         // Set the output file path
-                        string outputFilePath = saveFileDialog.FileName;
+                        _outputFilePath = saveFileDialog.FileName;
                         //string outputFilePath = saveFileDialog.FileName;
                         labelStatus.ForeColor=Color.Blue;
                         labelStatus.Text = "Video Converting !";
-                                                
 
-                            // FFmpeg Definitions Download and convert to MP3
-                            await youtube.Videos.DownloadAsync(videoUrl, outputFilePath, options => options
-                                .SetContainer(_format) // Set the desired format
-                                .SetFFmpegPath("c:/apagar/ffmpeg.exe")); // Set the path to ffmpeg if not in PATH
+                        // Convert Process
+                        YoutubeConverter(videoUrl,_outputFilePath,_format,_ffmpegPath);
+                        await Task.Delay(1000);
 
+                        //Change Color Labels
                         labelStatus.ForeColor = Color.Red;
                         labelStatus.Text = "Download completed!";
-                        await Task.Delay(5000);
+                        await Task.Delay(3000);
                         textBox1.Clear();
                         labelStatus.Text = "-";
                         labelStatus.ForeColor = Color.Black;
-
-                        // Open Folder in the end
-                        if (checkBox1.Checked) {                            
-                            string directoryPath = System.IO.Path.GetDirectoryName(outputFilePath);
-                            // Open the directory in File Explorer
-                            System.Diagnostics.Process.Start("explorer.exe", directoryPath);
-                        }                     
-
                         
                     }
                 }
                 catch (Exception ex)
+                {                    
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }       
+        
+        }
+
+        private async void YoutubeConverter(string videoUrl, string outputFilePath,string _format, string _ffmpegPath)
+        {
+            //Converted
+            var youtube = new YoutubeClient();
+            // FFmpeg Definitions Download and convert to MP3
+            await youtube.Videos.DownloadAsync(videoUrl, outputFilePath, options => options
+                .SetContainer(_format) // Set the desired format                                       
+                .SetFFmpegPath(_ffmpegPath)); // Set the path to ffmpeg if not in PATH
+
+            // For using Normalization of volume
+            if (checkBoxNormalize.Checked)
+            {
+                string normalizedOutputPath = Path.ChangeExtension(outputFilePath, null) + "_normalized." + _format;
+
+                NormalizeVolume(outputFilePath, normalizedOutputPath, _ffmpegPath);
+
+                // Step 3: Optionally, replace the original file with the normalized version
+                File.Delete(outputFilePath);
+                File.Move(normalizedOutputPath, outputFilePath);
+            }
+
+        }
+
+        private void NormalizeVolume(string inputFilePath, string outputFilePath, string ffmpegPath)
+        {
+            // FFmpeg arguments for loudness normalization
+            string arguments = $"-i \"{inputFilePath}\" -af loudnorm=I=-14:TP=-2:LRA=11 \"{outputFilePath}\"";
+
+            // Create and start the FFmpeg process
+            using (var ffmpegProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
                 {
-                    labelStatus.Text = $"Error: {ex.Message}";
+                    FileName = ffmpegPath,
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            })
+            {
+                ffmpegProcess.Start();
+
+                // Capture output and error (optional)
+                string output = ffmpegProcess.StandardOutput.ReadToEnd();
+                string error = ffmpegProcess.StandardError.ReadToEnd();
+
+                ffmpegProcess.WaitForExit();
+
+                // Check for errors
+                if (ffmpegProcess.ExitCode != 0)
+                {
+                    throw new Exception($"FFmpeg normalization error: {error}");
                 }
             }
         }
 
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Make ComboBox Inicial to mp3 format
             comboBox1.SelectedIndex = 0;
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_outputFilePath == null)
+            {
+                MessageBox.Show("Please convert a file");
+            }
+            else 
+            {
+                string directoryPath = System.IO.Path.GetDirectoryName(_outputFilePath);
+                System.Diagnostics.Process.Start("explorer.exe", directoryPath);
+            }
+
+        }
     }
 }
